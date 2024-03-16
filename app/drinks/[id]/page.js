0eb56106cdd4b6a4eb2b3597f16717a64ac2e85f
Vamonosraza <martinezjessy393@gmail.com>
2024-03-16 12:37:06 -0700

    Static images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import drinkImg from './pexelsDrink.jpeg';
+console.log(drinkImg);
+import Image from 'next/image'
 
 
 const getSIngleDrink = async (id) => {
@@ -18,6 +21,7 @@ const SingleDrinkPage = async ({params}) => {
   const imgSrc = data?.drinks[0]?.strDrinkThumb || ''
   return <div>
     <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>Back to drinks</Link>
+    <Image src={drinkImg} alt='drink' className='w-48 h-48 rounded-lg'/>
       <h1 className='text-4xl mb8title'>{title}</h1>
       <img src={imgSrc} alt={title} />
   </div>
