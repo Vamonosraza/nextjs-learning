30aca978f533a2ea861b9d99367abbef3f5cd390
Vamonosraza <martinezjessy393@gmail.com>
2024-03-16 12:21:40 -0700

    displaying the drink image

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,10 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
-const SingleDrinkPage = ({params}) => {
-    console.log(params.id)
-  return (
-    <div>SingleDrinkPage</div>
-  )
+
+const getSIngleDrink = async (id) => {
+    const response = await fetch(`${url}${id}`)
+    if(!response.ok){
+        throw new Error('Failed to fetch data')
+    }
+    return response.json()
 }
+
+const SingleDrinkPage = async ({params}) => {
+  const data = await getSIngleDrink(params.id);
+  console.log(data);
+  const title = data?.drinks[0]?.strDrink || 'No name'
+  const imgSrc = data?.drinks[0]?.strDrinkThumb || ''
+  return <div>
+    <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>Back to drinks</Link>
+      <h1 className='text-4xl mb8title'>{title}</h1>
+      <img src={imgSrc} alt={title} />
+  </div>
+};
 
 export default SingleDrinkPage
