4d69bea03ce929ff6dc3bfa86896223222afeebc
Vamonosraza <martinezjessy393@gmail.com>
2024-05-25 09:59:44 -0700

    did form with asynch function

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,8 +1,29 @@
+import { data } from 'autoprefixer';
+import { revalidatePath } from 'next/cache';
 import React from 'react'
+
+const createTask = async (formData) => {
+    'use server'
+    const content = formData.get('content');
+    
+    await prisma.task.create({
+        data:{
+            content,
+        },
+    });
+    revalidatePath('/task');
+};
 
 const TaskForm = () => {
   return (
-    <div>TaskForm</div>
+    <form action={createTask}>
+    <div className='join w-full'>
+        <input type='text' className='input input-border join-item w-full' placeholder='type your task here...' name='content'/>
+        <button type='submit' className='btn btn-primary join-item'>
+            Create Task
+        </button>
+    </div>
+    </form>
   )
 }
 
