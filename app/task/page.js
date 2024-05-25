122b128d2eb821438af9d39a72a5acea74b9a3e4
Vamonosraza <martinezjessy393@gmail.com>
2024-05-24 20:00:53 -0700

    Task page is fixed. Some typos were sending errors

--- app/task/page.js
+++ app/task/page.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import TaskForm from '@/components/TaskForm'
+import TaskList from '@/components/TaskList'
 
 const task = () => {
   return (
-    <div>
-        <h1 className='text-7xl'>Task Page</h1>
-        <Link href='/' className='text-2xl m-2'>Home Page</Link>
-        <Link href='/about/info' className='text-2xl m-2'>Info Page</Link>
+    <div className='max-w-lg'>
+        <TaskForm />
+        <TaskList />
     </div>
   )
 }
---
+++ components/TaskList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DeleteForm from "./DeleteForm";
+import Link from "next/link";
+
+const TaskList = async () => {
+  const tasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (tasks.length === 0) {
+    return <h2 className="mt-8 font-medium text-lg">No task to show...</h2>;
+  }
+
+  return (
+    <ul className="mt-8">
+      {tasks.map((task) => {
+        return (
+          <li
+            key={task.id}
+            className="flex justify-between items-center px-6 py-4 mb-4 border border-base-300 rounded-ls shadow-lg"
+          >
+            <h2
+              className={`text-lg capitalize ${
+                task.completed ? "line-through" : null
+              }`}
+            >
+              {task.content}
+            </h2>
+            <div className="flex gap-6 item-center">
+              <Link href={`/task/${task.id}`} className="btn btn-accent btn-xs">
+                Edit
+              </Link>
+              <DeleteForm id={task.id} />
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TaskList;
