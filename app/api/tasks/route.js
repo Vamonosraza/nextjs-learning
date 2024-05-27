5896ad26d11525890dd0fa04f22e84d16cf3c573
Vamonosraza <martinezjessy393@gmail.com>
2024-05-26 23:37:58 -0700

    added middleware

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,9 +1,26 @@
 import db from '@/utils/db';
+import { NextResponse } from 'next/server';
 
 export const GET = async(request) =>{
     const tasks = await db.task.findMany()
 
-    return Response.json({
+    return NextResponse.json({
         data: tasks
+    })
+};
+
+export const POST = async(request) =>{
+    // const tasks = await db.task.findMany()
+    // console.log(request)
+    const data = await request.json()
+    const task = await db.task.create({
+        data:{
+            content: data.content,
+
+        }
+    })
+
+    return Response.json({
+        data: task
     })
 };
