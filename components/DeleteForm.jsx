c586c8e1d673561b3d1ae792a07e65a87ecaf3d2
Vamonosraza <martinezjessy393@gmail.com>
2024-05-26 20:33:21 -0700

    pending on DeleteForm

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,11 +1,23 @@
+'use client'
 import React from 'react'
 import { deleteTask } from '@/utils/actions'
+import { useFormStatus } from 'react-dom'
+
+const SubmitButton = () => {
+  const {pending} = useFormStatus();
+  return (
+    <button type='submit' className='btn btn-xs btn-error' disabled={pending}>
+      {pending ? 'Please Wait...' : 'Delete'}
+    </button>
+  )
+
+}
 
 const DeleteForm = ({id}) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-xs btn-error'>Delete</button>
+      <SubmitButton/>
     </form>
   )
 }
